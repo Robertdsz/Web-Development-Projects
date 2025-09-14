c5715ec9cabd58ab4e7a73211b858fc298b6f809
robertdsz <randondesigner19@gmail.com>
2025-09-14 15:13:15 -0300

    Simon Game add new functions

--- Simon+Game+Project/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Project/Simon Game Challenge Starting Files/game.js
@@ -1,8 +1,15 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
+var level = 0;
 
 function nextSequence(){
+    
+    level ++;
+
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4);
     
     var randomChosenColour = buttonColours[randomNumber];
@@ -11,6 +18,7 @@ function nextSequence(){
 
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
+
 }
 
 function playSound(color){
@@ -22,8 +30,24 @@ $(".btn").on("click", function(){
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
+    animatePress(userChosenColour);
 })
 
 function animatePress(currentColor){
-    
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function(){
+        $("#" + currentColor).removeClass("pressed");
+    }, 80);
 }
+
+$(document).keyDown(function(){
+    
+    if(!started){
+
+        $("#level-title").text("Level " + level);
+        
+        nextSequence();
+        started = true;
+    }
+})
