13177ea30736bfc337728ca38e58047e092f1afc
robertdsz <randondesigner19@gmail.com>
2025-12-05 14:24:46 -0300

    Challenge Middleware Finish

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -8,14 +8,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 const password = 'iLoveProgramming';
-const passwordStatus = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 function checkPassword(req, res, next) {
-    if (req.body["password"] === password) {
-        passwordStatus = true;
-    }
+    req.passwordStatus = (req.body.password === password);
     next();
 }
 
@@ -24,12 +21,14 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.use(checkPassword);
-
-app.post('/check', (req, res) => {
-    if (passwordStatus) {
+app.post('/check', checkPassword, (req, res) => {
+    if (req.passwordStatus) {
         res.sendFile(__dirname + '/public/secret.html');
     } else {
         res.sendFile(__dirname + '/public/index.html');
     }
 });
+
+app.listen(port, () => {
+    console.log("Port 3000")
+})
